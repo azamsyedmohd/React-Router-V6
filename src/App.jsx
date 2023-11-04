614bc1195137d8813fb9b9a2634275ccf65015d8
Syed Mohd Azam <smazamamu@gmail.com>
2023-11-04 13:13:15 +0530

    Components Added and Routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router";
+import Home from "./Components/Home";
+import About from "./Components/About";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
